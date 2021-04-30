7eb70d17c7b4e0f380d9cca79974ba38370d2aed
KhangLD5 <KhangLD5@fpt.com.vn>
2021-04-30 20:21:28 +0700

    apache kafka

---
+++ spring-kafka-examples/src/main/java/com/example/springkafka/Foo1.java
@@ -0,0 +1,12 @@
+package com.example.springkafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foo1 {
+    @Getter
+    private String foo;
+}
---
+++ spring-kafka-examples/src/test/java/com/example/springkafka/simpleTest/SpringKafkaExamplesApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.springkafka.simpleTest;
+
+import com.example.springkafka.Foo1;
+import com.example.springkafka.simpleTest.KafkaConsumer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+class SpringKafkaExamplesApplicationTests {
+
+    @Autowired
+    KafkaTemplate<Object, Object> template;
+
+    @Autowired
+    KafkaConsumer consumer;
+
+    @Test
+    void checkMessage() throws InterruptedException {
+        this.template.send("topic1", new Foo1("Hello khang1"));
+
+        boolean isDone = consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        if (isDone) {
+            assertFalse(consumer.getPayload().isEmpty());
+        }
+    }
+}
