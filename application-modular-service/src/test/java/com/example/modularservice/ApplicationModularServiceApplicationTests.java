d92cae80e5fadc21a264c0ff0ca4f0a7e59387e4
KhangLD5 <KhangLD5@fpt.com.vn>
2021-05-01 10:01:58 +0700

    init spring contract verifier

---
+++ application-modular-service/src/test/java/com/example/modularservice/ApplicationModularServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modularservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationModularServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
