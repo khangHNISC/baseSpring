4dd053ec7e294cb80add35d5400bc366cd19b399
KhangLD5 <KhangLD5@fpt.com.vn>
2021-05-02 10:18:48 +0700

    spring security

---
+++ secured-application/src/main/java/com/example/securedapplication/Controller.java
@@ -0,0 +1,13 @@
+package com.example.securedapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Controller {
+
+    @GetMapping("/private")
+    public String hello() {
+        return "hello";
+    }
+}
