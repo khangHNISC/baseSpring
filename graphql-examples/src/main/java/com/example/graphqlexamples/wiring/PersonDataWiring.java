bc34a0df8ee25d6bf45b79f01ec4c52989eb9a02
khangHNISC <59554204+khangHNISC@users.noreply.github.com>
2021-05-05 16:38:41 +0700

    refactor code graphql:

--- graphql-examples/src/main/java/com/example/graphqlexamples/wiring/PersonDataWiring.java
+++ graphql-examples/src/main/java/com/example/graphqlexamples/wiring/PersonDataWiring.java
@@ -1,17 +1,15 @@
-package com.example.graphqlexamples;
+package com.example.graphqlexamples.wiring;
 
 import com.example.graphqlexamples.services.PersonService;
 import graphql.schema.idl.RuntimeWiring;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;;
 import org.springframework.graphql.boot.RuntimeWiringCustomizer;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class PersonDataWiring implements RuntimeWiringCustomizer {
-    @Autowired
-    private PersonService personService;
+    private final PersonService personService;
 
     @Override
     public void customize(RuntimeWiring.Builder builder) {
