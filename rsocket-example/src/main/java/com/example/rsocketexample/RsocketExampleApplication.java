f980983836ee197e6aff001f889c0b96a2ba5992
KhangLD5 <KhangLD5@fpt.com.vn>
2021-05-15 15:26:16 +0700

    init rsocket

---
+++ rsocket-example/src/main/java/com/example/rsocketexample/RsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketExampleApplication.class, args);
+    }
+
+}
---
+++ rsocket-example/src/test/java/com/example/rsocketexample/RsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
