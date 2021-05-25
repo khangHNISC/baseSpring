0f8d65e22d18b37d87048d76038f87d5825cdcb7
KhangLD5 <KhangLD5@fpt.com.vn>
2021-05-25 14:41:01 +0700

    add docker image build

--- spring-docker/app/src/main/java/com/example/app/AppApplication.java
+++ spring-docker/app/src/main/java/com/example/app/AppApplication.java
@@ -2,9 +2,17 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AppApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
